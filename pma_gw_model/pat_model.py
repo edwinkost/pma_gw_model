4720d9394516d720486938e041cea67107067cf8
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2018-09-25 12:09:49 +0200

    let's try

--- pma_gw_model/pat_model.py
+++ pma_gw_model/pat_model.py
@@ -183,6 +183,8 @@ class PantaiAirTanahModel(DynamicModel, MonteCarloModel):
         self.tide_water_level = tide_amplitude * np.sin( (2.0 * np.pi * self.timestep_in_day / (tide_periode_in_day )) )
         #
         
+        print self.time_and_tide[self.time_step_index-1].split()[1]
+        
         # TODO: Read this from the file
         self.tide_water_level = 0.5 * (self.time_and_tide[self.time_step_index-1].split()[1] + self.time_and_tide[self.time_step_index].split()[1])
         print(self.tide_water_level)
